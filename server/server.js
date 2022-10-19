a3a2ce838a5d6a3249acb6861fb2e93faf42751f
sinsu <sinsu19012@gmail.com>
2022-10-19 21:38:31 +0900

    초기 세팅

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path =require("path");
+const app = express();
+const port = 5000;
+
+app.use(express.static(path.join("../client/","build")));
+app.listen(port,()=>{
+    console.log("서버가 정상적으로 실행 되었습니다.");
+});
+app.get("*",(req,rep)=>{
+    rep.sendFile(path.join("../client/","build/index.html"));
+    
+});
