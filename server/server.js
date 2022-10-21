c2574f2a36eeaf96469450eef3c970e57e98fd8d
sinsu <sinsu19012@gmail.com>
2022-10-21 19:01:06 +0900

    refactor : api코드 재구성,contract abi,코드 추가

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path =require("path");
 const app = express();
 const port = 5000;
 
-
+app.use(express.json());
 app.use(express.static(path.join("../client/","build")));
 app.listen(port,()=>{
     console.log("서버가 정상적으로 실행 되었습니다.");
