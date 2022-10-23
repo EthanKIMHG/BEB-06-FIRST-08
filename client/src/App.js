71d01e5ff50fe65fbbd5da0b6a77fc1db15f78be
EthanKIM <khg023246@gmail.com>
2022-10-23 11:52:24 +0900

    feat: section2, section2list

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,41 @@
-import react, {useState} from 'react';
-import Header from './components/Header';
-import Section from './components/Section';
-import Section1 from './components/Section1';
-import Section2 from './components/Section2';
-import Section3 from './components/Section3';
-import Section4 from './components/Section4';
+import react, {useState, useEffect} from 'react';
 import MainPage from './pages/MainPage';
 import { Routes, Route } from "react-router-dom";
 import CreatePage from './pages/CreatePage';
+import Web3 from 'web3';
+import axios from "axios"
 
+// MainPage에 넘겨줄 db list
+// 여기에 만들면, page별로 다 뿌릴 수 있다.
 
 function App() {
 
   //연결된 account의 주소값.
   const [account, setAccount] = useState('')
+  const [collectionList, setCollectionList] = useState([]);
+  const [web3, setWeb3] = useState();
+
+
+  useEffect(() => {
+      if (typeof window.ethereum !== "undefined") { // window.ethereum이 있다면
+          try {
+              const web = new Web3(window.ethereum);  // 새로운 web3 객체를 만든다
+              setWeb3(web);
+          } catch (err) {
+              console.log(err);
+          }
+      }
+    }, []);
+
+
+  //첫 페이지 렌더링시 NFT 리스트 가져오기. 그리고 collectionList를 하위 컴포넌트로 보냅니다.
+  
+  useEffect(() => {
+/*     axios.get("http://localhost:5000/api/nftload")
+    .then(res => {
+      setCollectionList(rea.data);
+    }).catch(e => {console.log(e)}) */
+  },[])
 
   //wallet connect 기능
   const handleWalletClick = async () => {
@@ -23,9 +45,16 @@ function App() {
     setAccount(accounts[0]);
   }
 
+
   return (
     <Routes>
-      <Route path='/' element={<MainPage handleWalletClick={handleWalletClick} account={account}/>} />
+      <Route path='/'element={
+        <MainPage
+          handleWalletClick={handleWalletClick}
+          account={account}
+          collectionList={collectionList}
+        />
+      }/>
       <Route path='/create' element={<CreatePage handleWalletClick={handleWalletClick} account={account}/>} />
     </Routes>
   )
--- client/src/components/Section1.js
+++ client/src/components/Section1.js
@@ -4,10 +4,10 @@ import "../utils/Section1.css";
 function Section1() {
   return (
     <div className="section-1">
-      <div className='fresnel-container fresnel-greaterThanOrEqual-xxl'>
+      <div className='fresnel-container fresnel-greaterThanOrEqual-sm'>
         <div className="section1-container"> {/*grid가 이미 적용.*/}
           <div className='section1-container2'>
-            <div className="fresnel-container fresnel-greaterThanOrEqual-xxl">
+            <div className="fresnel-container fresnel-greaterThanOrEqual-sm">
               <div className="section1-wrapper">
                 <div className='section1-text'>
                   <h1 className="text">Explore, collect, and sell NFTs</h1>
@@ -19,11 +19,10 @@ function Section1() {
             <div className="background-grid">
               <div className='background-container'>
                 <div className='background-wrapper'>
-                  <div className='fresnel-container fresnel-lessThanOrEqual-xxl'>
-                    <div className='background-row' style={{display: "flex", justifyContent: "center", height: "50%"}}>
-                      <img src="https://w0.peakpx.com/wallpaper/652/786/HD-wallpaper-arid-desert-landscape-at-twilight-sky-clouds-cactus-twilight-deserts-sunsets-nature.jpg" style={{ display: "block", width: "auto", height:"auto", borderRadius:"10px"}} />
-                      <img src="https://w0.peakpx.com/wallpaper/652/786/HD-wallpaper-arid-desert-landscape-at-twilight-sky-clouds-cactus-twilight-deserts-sunsets-nature.jpg" style={{ display: "block", width: "auto", height:"auto", borderRadius:"10px"}} />
-                    </div>
+                  <div className='background-row'>
+                    <img src="https://images.pexels.com/photos/1703317/pexels-photo-1703317.jpeg?auto=compress&cs=tinysrgb&w=1600"/>
+                    <img src="https://w0.peakpx.com/wallpaper/652/786/HD-wallpaper-arid-desert-landscape-at-twilight-sky-clouds-cactus-twilight-deserts-sunsets-nature.jpg" />
+                    <img src="https://images.pexels.com/photos/34107/milky-way-stars-night-sky.jpg?auto=compress&cs=tinysrgb&w=1600" />
                   </div>
                 </div>
               </div>
