7a73db6d219f4f22fbb82e53d512fca102cbf19d
sinsu <sinsu19012@gmail.com>
2022-10-25 17:02:12 +0900

    refactor : client 코드 리팩토링

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import react, {useState, useEffect, useContext, createContext} from 'react';
+import {useState, useEffect} from 'react';
 import MainPage from './pages/MainPage';
 import { Routes, Route } from "react-router-dom";
 import CreatePage from './pages/CreatePage';
@@ -6,7 +6,8 @@ import AccountPage from './pages/AccountPage';
 import Web3 from 'web3';
 import axios from "axios"
 import { AppContext } from './AppContext';
-import {NFTStorage,File} from 'nft.storage';
+import {NFTStorage} from 'nft.storage';
+import Swal from 'sweetalert2';
 import abi from './abi/abi.json';
 
 
@@ -21,12 +22,25 @@ function App() {
   const web3 = window.ethereum ? new Web3(window.ethereum) : null;
   const contract = web3 ? new web3.eth.Contract(abi,'0xa7f945f986f244A794c84B0270Eb35E1Aa91Bd87'):null;
   const client = new NFTStorage({ token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGU3MmYyYzMyMUUzZmEwMmU4MDlkODFhYWJhOWRFMjg3NjNGMUEyNWIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2NjUwMTY1ODM0MCwibmFtZSI6InNpbnN1In0.BlSLUWAZMaXtDKs43HTrxBhypSTcU-gSIFee-nRME18'});
-
+  
   const handleWalletClick = async () => {
-    let accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    setAccount(accounts[0]);
+    try{
+      let accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setAccount(accounts[0]);
+    }catch(e){
+      Swal.fire({
+        title: '메타마스크 연결을 확인해주세요.',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
+    }
+    
   }
 
   const context = {
--- client/src/components/Section.js
+++ client/src/components/Section.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Section1 from './Section1'
 import Section2 from './Section2'
-import Section3 from './Section3'
-import Section4 from './Section4'
 import "../utils/Section.css"
 
 
@@ -10,13 +8,10 @@ import "../utils/Section.css"
 function Section() { // collectionList를 section2에 넘겨줍니다.
   return (
     <main id="main">
-      <div class="section">
+      <div className="section">
         <div className='section-grid'> {/*gird를 기준으로 section이 나눔.*/}
           <Section1 />
           <Section2 />
-          {/*
-          <Section3 />
-          <Section4 /> */}
         </div>
       </div>
     </main>
